8989e1c999caebcc025d62e8c638f01eaf883a24
juancruzmoran <juancruzmoran@outlook.es>
2022-06-30 19:39:12 -0300

    iniciamos el servidor

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const port = 3030;
+
+const app = express ();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+
+
+app.listen(port, () => console.log('Servidor corriendo en http://localhost:' + port))
